b3839acf02e670dcfb21c24b2447adca48c352b2
Beshr Kayali <me@beshr.com>
2021-09-26 23:03:21 +0200

    a few more modes

--- init.el
+++ init.el
@@ -57,12 +57,18 @@
 	 typescript-mode
 	 unicode-fonts
 	 auctex
+	 dockerfile-mode
+	 docker-compose-mode
+	 meson-mode
 	 ))
 
 (setq warning-suppress-log-types
       '((comp)))
 
 
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/custom/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
 
 ;; Setup things
 (add-hook
